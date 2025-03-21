0e07f69aee8852a396faac21fc2f893b1f862fd9
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-21 18:29:37 +0700

    new commit player dying 2.0

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 func _on_body_entered(body: Node2D) -> void:
 	print("YOU DIED!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	body.coin_total()
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
